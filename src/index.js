bad8a709dd90b361de381e21a95aba3e763375cf
sebast825 <sebamolina825@gmail.com>
2022-12-20 11:14:58 -0300

    btn works

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ const preloader = document.querySelector('.preloader');
 window.addEventListener('load',()=>{
   preloader.style.display = 'none';
 })
+
+
+
+const checkboxLanguage = document.querySelector('.checkboxLanguage');
+
+
+checkboxLanguage.addEventListener('click',()=>{
+  let checkboxValue = checkboxLanguage.checked;
+
+  if (checkboxValue){
+    location.href = "spanish.html"
+  }else{
+    location.href = "index.html"
+  }
+})
